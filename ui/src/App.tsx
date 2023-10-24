342959fbac4cbefaa0dde23f5dc70171de2ff885
Peter <peter@interlay.io>
2023-10-24 11:39:16 +0200

    chore: lint

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,13 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components';
 import Wallet from './pages/Wallet/Wallet';
 
-
 function App() {
   return (
     <Layout>
       <BrowserRouter>
         <Routes>
-          <Route element={<Wallet/>} path='/' />
+          <Route element={<Wallet />} path='/' />
         </Routes>
       </BrowserRouter>
     </Layout>
--- ui/src/pages/Wallet/Wallet.tsx
+++ ui/src/pages/Wallet/Wallet.tsx
@@ -1,17 +1,17 @@
-import { AssetsTable } from './AssetsTable'
+import { AssetsTable } from './AssetsTable';
 import { Header } from '../../components/Layout/Header';
 import { useConnect } from '../../hooks/useConnect';
 import { useBalances } from '../../hooks/useBalances';
 
 const Wallet = () => {
   const { connect, evmAccount, bitcoinAddress, publicClient, walletClient } = useConnect();
-  const {balances, refetchBalances} = useBalances(evmAccount, bitcoinAddress, publicClient);
+  const { balances, refetchBalances } = useBalances(evmAccount, bitcoinAddress, publicClient);
   return (
     <div>
-      <Header {...{connect, evmAccount, bitcoinAddress}} />
-      <AssetsTable {...{balances, refetchBalances, evmAccount, bitcoinAddress, walletClient}} />
-      </div>
-  )
-}
+      <Header {...{ connect, evmAccount, bitcoinAddress }} />
+      <AssetsTable {...{ balances, refetchBalances, evmAccount, bitcoinAddress, walletClient }} />
+    </div>
+  );
+};
 
-export default Wallet
+export default Wallet;
